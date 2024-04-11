7da9dc26724a1e5bce3e849102725729cd3df445
cesarr340 <cesarr340@gmail.com>
2024-04-11 18:36:20 +0000

    adicionar um componente de menu lateral

---
+++ src/components/Menu.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import '../styles/menu.scss'
+
+export function Menu() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <label className="hamburger" onClick={handleShow}>
+        <input type="checkbox" />
+        <svg viewBox="0 0 32 32">
+          <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+          <path className="line" d="M7 16 27 16"></path>
+        </svg>
+      </label>
+
+      {/* <Button className='menu-button' variant="primary" onClick={handleShow}>
+        launch
+      </Button> */}
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+export default Menu;
